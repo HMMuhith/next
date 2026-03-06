869d7f5c0ace6ba38e7444158fb4ed21aa03bc16
Muhith <muhithkhan8@gmail.com>
2026-03-06 15:48:20 +0600

    carousel changes

--- Next/src/app/components/carousel.js
+++ Next/src/app/components/carousel.js
@@ -1,56 +1,56 @@
 'use client'
-import React, { useEffect, useRef, useState } from 'react'
-import Image from 'next/image'
+import React,{ useEffect, useRef, useState } from 'react'
+import image1 from '../../../public/71SRwYaVx3L._SX3000_.jpg'
+import image2 from '../../../public/71t6-l+UneL._SX3000_.jpg'
+import image3 from '../../../public/71z3-+jDtcL._SX3000_.jpg'
+import image4 from '../../../public/81DKIMqhSQL._SX3000_.jpg'
+import image5 from '../../../public/81H-rqTT6QL._SX3000_.jpg'
+import image6 from '../../../public/81jDd5S9ewL._SX3000_.jpg'
+import image7 from '../../../public/81YK5C5+3vL._SX3000_.jpg'
+import image8 from '../../../public/B1Kw+ZArijL._SX3000_.jpg'
+import Image from 'next/image';
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
 
-const Images = [
-  '/71SRwYaVx3L._SX3000_.jpg',
-  '/71t6-l+UneL._SX3000_.jpg',
-  '/71z3-+jDtcL._SX3000_.jpg',
-  '/81DKIMqhSQL._SX3000_.jpg',
-  '/81H-rqTT6QL._SX3000_.jpg',
-  '/81jDd5S9ewL._SX3000_.jpg',
-  '/81YK5C5+3vL._SX3000_.jpg',
-  '/B1Kw+ZArijL._SX3000_.jpg'
-]
 
-const Carousel = () => {
-  const [Index, setIndex] = useState(0)
-  const intervalRef = useRef(null)
 
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      setIndex(prev => (prev + 1) % Images.length)
-    }, 4000)
+const Images=[image1,image2,image3,image4,image5,image6,image7,image8]
+const Carousel=()=>{
 
-    return () => clearInterval(intervalRef.current)
-  }, [])
+const [Index,setIndex]=useState(0)
 
-  const prev = () => setIndex(prev => (prev - 1 + Images.length) % Images.length)
-  const next = () => setIndex(prev => (prev + 1) % Images.length)
+const intervalref=useRef(null)
 
-  return (
-    <div className="relative flex justify-center items-center w-full h-150">
-      <Image
-        src={Images[Index]}
-        alt={`Slide ${Index}`}
-        width={1000}
-        height={600}
-        className="object-contain"
-      />
+const startInterval=()=>{
+    intervalref.current=setInterval(()=>{
+        setIndex((prev)=>(prev+1)%Images.length)
+    },4000)
+}
+const resetInterval=()=>{
+    clearInterval(intervalref.current)
+    startInterval()
+}
 
-      <div className="absolute top-1/2 left-10 -translate-y-1/2 cursor-pointer" onClick={prev}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
-          <path d="M15 18l-6-6 6-6" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-        </svg>
-      </div>
+useEffect(()=>{
+    startInterval()
+    return ()=>clearInterval(intervalref.current)
+    },[Images.length])
 
-      <div className="absolute top-1/2 right-10 -translate-y-1/2 cursor-pointer" onClick={next}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
-          <path d="M9 18l6-6-6-6" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-        </svg>
-      </div>
+const prev=()=>{setIndex(prev=>(prev-1+Images.length)%Images.length);resetInterval()}
+const next=()=>{setIndex(prev=>(prev+1)%Images.length);resetInterval()}
+return (<>
+<div>
+    <div className='relative flex justify-center items-center'>
+    <Image src={Images[Index]} width={1000} alt='slide'/>
     </div>
-  )
+    <div className='absolute top-56 right-56 cursor-pointer'>
+        <GrNext onClick={next} className='text-white'/>
+    </div>
+    <div className='absolute top-56 left-56 cursor-pointer'>
+<GrPrevious onClick={prev} className='text-white'/>
+    </div>
+</div>
+</>)
 }
 
 export default Carousel
